dd5a9e21b17c30ad4201742746ce26a5b8118281
TheoMuffin <TheoMuffin@users.noreply.github.com>
2018-03-15 10:34:32 +0200

    Tegin looksulgude formati javale kohaseks

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
 
         String seis =
                 "----------\n" +
@@ -14,18 +12,17 @@ public class Main
                         "XXX#---X\n" +
                         "XXX---XX\n" +
                         "XXXXXXXX";
-        
+
         Tetris tetris = new Tetris(seis);
         Scanner sc = new Scanner(System.in);
 
         String tegevus = "";
-        do
-        {
+        do {
             System.out.println("Sisesta tegevus: ");
             tegevus = sc.nextLine();
-            seis = tetris.arvutaUusSeis(seis,tegevus);
+            seis = tetris.arvutaUusSeis(seis, tegevus);
             tetris.setSeis(seis);
         }
-        while(!tegevus.equals("exit")); //Stringide puhul tuleb kasutada .equals, mitte ==
+        while (!tegevus.equals("exit")); //Stringide puhul tuleb kasutada .equals, mitte ==
     }
 }
