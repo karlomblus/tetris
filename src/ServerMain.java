883aecdacf8f9cb26e8f72f8e19ffaccd077855e
karlomblus <37180265+karlomblus@users.noreply.github.com>
2018-03-21 04:29:47 +0200

    mõned mõtted

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,10 +1,13 @@
 public class ServerMain {
     public static void main(String[] args) {
 
+        // tekitame mingi hulga threade weebi ühendustega tegelemiseks
 
-        // kuulame porti
+        // kuulame porti (millist?)
         // connectioni peale tekitan threadi ja ServerGameConnectionHandler tegeleb
 
+        // lisaks kuulan ka porti 80
+        // kui tuleb ühendus, annan selle esimesele vabale threadile (ei hakka igakord uut tegema)
 
 
     }
