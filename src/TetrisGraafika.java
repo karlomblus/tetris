98dafab0d4d7b71255ab0bb5e0c4356522eeda23
TheoMuffin <TheoMuffin@users.noreply.github.com>
2018-03-22 19:27:01 +0200

    TetrisGraafika minimaalsed muudatused

--- src/TetrisGraafika.java
+++ src/TetrisGraafika.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
 
-public class TetrisGraafika {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class TetrisGraafika extends Application {
+
+    @Override
+    public void start(Stage peaLava) throws Exception {
+        Group juur = new Group(); // luuakse juur
+        int mitukuubikut = 50;
+        HashMap<Integer, Color> rectToColor = new HashMap<>();
+        Rectangle ristkülik[][] = new Rectangle[50][50];
+        int ruuduSuurus = 10;
+
+        int kuubikutLaiuses = mitukuubikut;
+        int kuubikutPikkuses = mitukuubikut;
+        for (int i = 0; i < kuubikutPikkuses; i++) {
+            for (int j = 0; j < kuubikutLaiuses; j++) {
+                ristkülik[i][j] = new Rectangle(j * ruuduSuurus, i * ruuduSuurus, ruuduSuurus, ruuduSuurus);
+                ristkülik[i][j].setFill(Color.BLUE);
+                System.out.println(50 * i + j);
+                rectToColor.put(50 * i + j, Color.BLUE);
+                ristkülik[i][j].setStroke(Color.BLUE);
+               // System.out.println(ristkülik[i][j].getFill());
+                juur.getChildren().add(ristkülik[i][j]);  // ristkülik lisatakse juure alluvaks
+            }
+        }
+
+
+
+        Scene stseen1 = new Scene(juur, 500, 500, Color.SNOW);  // luuakse stseen
+
+        peaLava.setTitle("Must ruut");  // lava tiitelribale pannakse tekst
+
+        peaLava.setScene(stseen1);  // lavale lisatakse stseen
+
+        peaLava.show();  // lava tehakse nähtavaks
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
