b33a34fc78f917532994ab20631944150aa10e95
TheoMuffin <TheoMuffin@users.noreply.github.com>
2018-04-02 13:46:51 +0300

    Liigutasin kogu tetromini funktsionaalsuse eraldi klassi ja staatus on nüüd chari põhine, mitte värvi. Samuti värv nüüd sõltub, mis tetrominoga tegemist

--- src/main/java/tetrispackage/TetrisGraafika.java
+++ src/main/java/tetrispackage/TetrisGraafika.java
@@ -11,8 +11,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class TetrisGraafika extends Application {
@@ -21,18 +19,8 @@ public class TetrisGraafika extends Application {
     final int ruuduSuurus = 15;
     final int mitukuubikutLaiuses = resoWidth / ruuduSuurus;
     final int mitukuubikutPikkuses = resoHeight / ruuduSuurus;
-    Rectangle ristkülik[][] = new Rectangle[mitukuubikutPikkuses][mitukuubikutLaiuses];
-    private Map<Integer, Color> rectToColor = new HashMap<>();
-    private int tetrominoDrawingAllowance = 2;
-    private boolean allowTetrominoDrawing = true;
-
-    private Color getRectColorAt(int i, int j){
-        return rectToColor.get(mitukuubikutLaiuses * i + j);
-    }
-    private Color setRectColorAt(int i, int j, Color color){
-        ristkülik[i][j].setFill(color);
-        return rectToColor.put(mitukuubikutLaiuses * i + j, color);
-    }
+    private Rectangle ristkülik[][] = new Rectangle[mitukuubikutPikkuses][mitukuubikutLaiuses];
+    Tetromino tetromino;
 
     @Override
     public void start(Stage peaLava) throws Exception {
@@ -40,23 +28,23 @@ public class TetrisGraafika extends Application {
         for (int i = 0; i < mitukuubikutPikkuses; i++) {
             for (int j = 0; j < mitukuubikutLaiuses; j++) {
                 ristkülik[i][j] = new Rectangle(j * ruuduSuurus, i * ruuduSuurus, ruuduSuurus, ruuduSuurus);
-                setRectColorAt(i, j, Color.BLUE);
                 ristkülik[i][j].setStroke(Color.RED);
                 juur.getChildren().add(ristkülik[i][j]);  // ristkülik lisatakse juure alluvaks
             }
         }
+        tetromino  = new Tetromino(ristkülik);
         Timeline fiveSecondsWonder = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
 
                 tick();
-                if (allowTetrominoDrawing){
-                    drawTetromino('Z');
+                if (tetromino.isDrawingAllowed()){
+                    tetromino.draw('S');
                 }
             }
         }));
         peaLava.setOnShowing(event -> { //Do only once
-            //drawTetromino('I');
+            //draw('I');
         });
         fiveSecondsWonder.setCycleCount(Timeline.INDEFINITE);
         fiveSecondsWonder.play();
@@ -66,60 +54,26 @@ public class TetrisGraafika extends Application {
         peaLava.show();  // lava tehakse nähtavaks
 
     }
-    //Tetriminos I O T J L S Z
-    void drawTetromino(char tetrominoType){
-        if (tetrominoType == 'I') {
-            int i = 0;
-            int j = 0;
-            setRectColorAt(i, j, Color.RED);
-            setRectColorAt(i, j+1, Color.RED);
-            setRectColorAt(i, j+2, Color.RED);
-            setRectColorAt(i, j+3, Color.RED);
-            tetrominoDrawingAllowance -= 1;
-        }
-        else if (tetrominoType == 'O'){
-            setRectColorAt(0, 0, Color.RED);
-            setRectColorAt(0, 1, Color.RED);
-        }
-        else if (tetrominoType == 'Z'){
-            if (tetrominoDrawingAllowance == 1) {
-                setRectColorAt(0, 1, Color.RED);
-                setRectColorAt(0, 2, Color.RED);
-            }
-            else{
-                setRectColorAt(0, 0, Color.RED);
-                setRectColorAt(0, 1, Color.RED);
-            }
-        }
-        else if (tetrominoType == 'S'){
-            if (tetrominoDrawingAllowance == 1) {
-                setRectColorAt(0, 0, Color.RED);
-                setRectColorAt(0, 1, Color.RED);
-            }
-            else{
-                setRectColorAt(0, 1, Color.RED);
-                setRectColorAt(0, 2, Color.RED);
-            }
-        }
-    }
+
     void tick() {
         System.out.println("Ticked");
-        if (tetrominoDrawingAllowance == 0){
+        if (tetromino.tetrominoDrawingAllowance == 0){
             System.out.println("TetriminoDrawing false!");
-            allowTetrominoDrawing = false;
+            tetromino.allowTetrominoDrawing = false;
         }
-        else if (tetrominoDrawingAllowance == -4){//Esialgu lubab uut iga nelja sekundi tagamt
-            allowTetrominoDrawing = true;
-            tetrominoDrawingAllowance = 2;
+        else if (tetromino.tetrominoDrawingAllowance == -4){//Esialgu lubab uut iga nelja sekundi tagamt
+            tetromino.allowTetrominoDrawing = true;
+            tetromino.tetrominoDrawingAllowance = 2;
         }
-        tetrominoDrawingAllowance -= 1;
+        tetromino.tetrominoDrawingAllowance -= 1;
         for (int i = mitukuubikutPikkuses - 1; i >= 0; i--) {
             for (int j = mitukuubikutLaiuses - 1; j >= 0; j--) {
-                if (getRectColorAt(i, j).equals(Color.RED)) {
+                if (tetromino.getRectStatusAt(i, j) == 'A') {
+                    System.out.println("Found active");
                     if (i + 1 < mitukuubikutPikkuses){
-                        setRectColorAt(i+1, j, Color.RED);
+                        tetromino.setRectStatusAt(i + 1, j, 'A');
                     }
-                    setRectColorAt(i, j, Color.BLUE);
+                    tetromino.setRectStatusAt(i, j, 'B');
                 }
             }
         }
---
+++ src/main/java/tetrispackage/Tetromino.java
@@ -0,0 +1,82 @@
+package tetrispackage;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Tetromino {
+    private Map<Integer, Character> rectToStatus = new HashMap<>();
+    public int tetrominoDrawingAllowance = 2;
+    public boolean allowTetrominoDrawing = true;
+    private Color activeTetrominoColor;
+    Rectangle [][] ruudud;
+
+    public Tetromino(Rectangle [][] ristkülik){
+        ruudud = ristkülik;
+        for (int i = 0; i < ruudud.length; i++) {
+            for (int j = 0; j < ruudud[i].length; j++) {
+                setRectStatusAt(i, j, 'B'); //B for background
+            }
+        }
+    }
+    public char getRectStatusAt(int i, int j){
+        return rectToStatus.get(ruudud[i].length * i + j);
+    }
+    public void setRectStatusAt(int i, int j, char status){
+        System.out.println("Inside setRectStatusAt");
+        rectToStatus.put(ruudud[i].length * i + j, status);
+        if (status == 'B'){
+            ruudud[i][j].setFill(Color.BLUE);
+        }
+        else if (status == 'A'){
+            ruudud[i][j].setFill(activeTetrominoColor);
+        }
+    }
+    //Tetriminos I O T J L S Z
+    void draw(char tetrominoType){
+        if (tetrominoType == 'I') {
+            int i = 0;
+            activeTetrominoColor = Color.CYAN;
+            for (int j = 0; j < 4; j++) {
+                setRectStatusAt(i, j, 'A');
+            }
+            tetrominoDrawingAllowance -= 1;
+        }
+        else if (tetrominoType == 'O'){
+            activeTetrominoColor = Color.YELLOW;
+            setRectStatusAt(0, 0, 'A');
+            setRectStatusAt(0, 1, 'A');
+        }
+        else if (tetrominoType == 'Z'){
+            if (tetrominoDrawingAllowance == 1) {
+                activeTetrominoColor = Color.RED;
+                setRectStatusAt(0, 1, 'A');
+                setRectStatusAt(0, 2, 'A');
+            }
+            else{
+                setRectStatusAt(0, 0, 'A');
+                setRectStatusAt(0, 1, 'A');
+            }
+        }
+        else if (tetrominoType == 'S'){
+            activeTetrominoColor = Color.LIME;
+            if (tetrominoDrawingAllowance == 1) {
+                setRectStatusAt(0, 0, 'A');
+                setRectStatusAt(0, 1, 'A');
+            }
+            else{
+                setRectStatusAt(0, 1, 'A');
+                setRectStatusAt(0, 2, 'A');
+            }
+        }
+    }
+    public void setAllowTetrominoDrawing(boolean allowTetrominoDrawing) {
+        this.allowTetrominoDrawing = allowTetrominoDrawing;
+    }
+
+    public boolean isDrawingAllowed() {
+        return allowTetrominoDrawing;
+    }
+}
