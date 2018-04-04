5e92e43788a3cdf16ccbb7c3afc6bd0e786db0ed
Karl <karl@carlnet.ee>
2018-04-04 23:00:35 +0300

    kasutajakonto regamine ja sisselogimisel paroolikontroll OK

--- src/main/java/tetrispackage/TestClientDemo.java
+++ src/main/java/tetrispackage/TestClientDemo.java
@@ -8,17 +8,34 @@ public class TestClientDemo {
     public static void main(String[] args) throws Exception {
 
         System.out.println("Ühendume serveriga");
-        Socket socket = new Socket("tetris.carlnet.ee", 54321);
+        Socket socket = new Socket("127.0.0.1", 54321);
         try (DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
              DataInputStream dis = new DataInputStream(socket.getInputStream())) {
 
-            System.out.println("Loome uue konto");
-            dos.writeInt(1); // konto loomise käsk
+            //System.out.println("Loome uue konto");
+            //dos.writeInt(1); // konto loomise käsk
+            //dos.writeUTF("UusUsername");
+            //dos.writeUTF("UusParool");
+/*
+            System.out.println("Logime sisse vale kasutajakontoga");
+            dos.writeInt(2);
+            dos.writeUTF("UusUsername3333");
+            dos.writeUTF("UusParool3333");
+
+            System.out.println("Logime sisse vale parooliga");
+            dos.writeInt(2);
+            dos.writeUTF("UusUsername");
+            dos.writeUTF("UusParool3333");
+*/
+
+            System.out.println("Logime sisse õige parooliga");
+            dos.writeInt(2);
             dos.writeUTF("UusUsername");
             dos.writeUTF("UusParool");
 
-            loesisendit(dis); // väga halb lahendus, hetkel kiiruga/demoks
 
+            loesisendit(dis); // väga halb lahendus, hetkel kiiruga/demoks
+/*
             System.out.println("Küsime userlisti");
             dos.writeInt(3);
 
@@ -37,8 +54,8 @@ public class TestClientDemo {
 
             System.out.println("Ütleme viisakalt bye");
             dos.writeInt(4);
-
-            Thread.sleep(1000); // ootame serveri vastuse ära enne kui toru maha viskame; ei ole hea viis
+*/
+            Thread.sleep(2000); // ootame serveri vastuse ära enne kui toru maha viskame; ei ole hea viis
             loesisendit(dis); // väga halb lahendus, hetkel kiiruga/demoks
 
         } finally {
@@ -51,18 +68,22 @@ public class TestClientDemo {
     private static void loesisendit(DataInputStream dis) throws Exception {
         System.out.println("Loeme serveri vastust");
         while (dis.available() > 0) {  // seda konstruktsiooni ei soovitatud vist üldse kasutada
-            switch (dis.readInt()) {
+            int servervastus = dis.readInt();
+            switch (servervastus) {
                 case 1:
                     System.out.println("Uue konto regamise vastus: status: " + dis.readInt() + ", message: " + dis.readUTF());
+                    break;
+                case 2:
+                    System.out.println("Sisselogimise vastus: status: " + dis.readInt() + ", message: " + dis.readUTF());
                     break;
                 case 3:
                     System.out.println("Userlist: uid: " + dis.readInt() + ", name: " + dis.readUTF());
                     break;
                 case 6:
-                    System.out.println("Running games: id: " + dis.readInt() + ", players: " + dis.readUTF()+ " ja " +dis.readUTF());
+                    System.out.println("Running games: id: " + dis.readInt() + ", players: " + dis.readUTF() + " ja " + dis.readUTF());
                     break;
                 default:
-                    System.out.println("Ma ei tea mis server ütles ja ei oska mitte midagi teha");
+                    System.out.println("Ma ei tea mis server ütles (" + servervastus + ") ja ei oska mitte midagi teha");
 
             }
         }
