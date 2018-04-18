3bd7afb526fb7d294dd81b5b9b7a22d3191114a1
Karl <karl@carlnet.ee>
2018-04-18 16:59:50 +0300

    weebiserver annab mingi 'viisaka' vastuse

--- src/main/java/tetrispackage/ServerWebThread.java
+++ src/main/java/tetrispackage/ServerWebThread.java
@@ -2,43 +2,66 @@ package tetrispackage;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.concurrent.BlockingQueue;
 
- class ServerWebThread implements Runnable {
+class ServerWebThread implements Runnable {
     BlockingQueue<Socket> socketid;
 
-     public ServerWebThread(BlockingQueue<Socket> socketid) {
-         this.socketid = socketid;
-     }
+    public ServerWebThread(BlockingQueue<Socket> socketid) {
+        this.socketid = socketid;
+    }
 
-     @Override
-     public void run() {
-         try {
-             while (true) {
+    @Override
+    public void run() {
+        try {
+            while (true) {
 
-                 Socket socket = socketid.take();
-                 ServerMain.debug(7,"thread võttis socketi");
-
-                 try (BufferedReader bis = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                     String inputLine;
-                     while ((inputLine = bis.readLine()) != null) {
-                         //instr.append(inputLine);
-                         System.out.println(inputLine);
-                         if (inputLine.equals(".q")) {
-                             socket.close();
-                             break;
-                         }
-                         if (inputLine.equals(".k")) throw new RuntimeException(); // test
-                     } // loeme ridu socketist
-                 } // try with resources
+                Socket socket = socketid.take();
+                ServerMain.debug(7, "thread võttis socketi");
 
 
-             } // WHILE
-         } catch (Exception e) {
-             throw new RuntimeException(e);
-         }
+                // PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+
+                try (BufferedReader bis = new BufferedReader(new InputStreamReader(socket.getInputStream())); PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+                    String inputLine;
+                    while ((inputLine = bis.readLine()) != null) {
+                        //instr.append(inputLine);
+                        System.out.println("'" + inputLine + "'");
+                        if (inputLine.equals(".q")) {
+                            socket.close();
+                            break;
+                        }
+                        if (inputLine.equals(".k")) throw new RuntimeException(); // test
+                        if (inputLine.equals("")) { // päring sai läbi
+                            System.out.println("saadame mingi kamarajura vastu");
+                            out.println("HTTP/1.1 200 OK");
+                            out.println("Server: Tetris scoreserver");
+                            out.println("Cache-Control: no-cache, no-store, must-revalidate");
+                            out.println("Pragma: no-cache");
+                            out.println("Connection: Close");
+                            out.println("Content-Type: text/html; charset=UTF-8");
+                            out.println("");
+                            out.println("<html><body>  Oled ühendunud tetrise serveri külge. <br><br>Varsti näed siin skoore ja saad mängu alla laadida. </body></html>");
+                            out.close();
+                            break;
+
+                        } // päring sai läbi
 
 
-     } // run
- }
+                    } // loeme ridu socketist
+                } // try with resources
+                finally {
+                    System.out.println("paneme socketi kinni");
+                    socket.close();
+                }
+
+            } // WHILE
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    } // run
+}
