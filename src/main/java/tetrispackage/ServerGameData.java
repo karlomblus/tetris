d039fb08d3c984b5431297d2562df8868b205f92
Karl <karl@carlnet.ee>
2018-04-24 20:48:32 +0300

    server genereerib küsimisel tetromino

--- src/main/java/tetrispackage/ServerGameData.java
+++ src/main/java/tetrispackage/ServerGameData.java
@@ -1,10 +1,7 @@
 package tetrispackage;
 
 import java.io.DataOutputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 public class ServerGameData {
     private Timer timer;
@@ -54,20 +51,55 @@ public class ServerGameData {
 
 
     private void tiksJuhtus() {
-        try {
+
             tickid++;
             for (ServerGameConnectionHandler player : players) {
+                try {
                 DataOutputStream dos = player.getDos();
                 synchronized (dos) {
                     dos.writeInt(100);
                     dos.writeInt(tickid);
                 } // sync
+                } catch (Exception e) { // kui üks mängija läheb katki, siis ei taha me õhku lennata vaid teine mängija jääb üksi mängima
+                    ServerMain.debug(1,"Tiksu saatmisel kasutajale "+player.getUsername()+" läks midagi valesti.");
+                    ServerMain.debug(5,e.toString());
+                    player.setOpponentID(0);
+                    players.remove(player); // teise mängija viskame minema
+                }
             } // iter
-        } catch (Exception e) {
-            ServerMain.error("Tiksu saatmisel läks midagi valesti",e); // antakse error edasi, logitakse ja lennatakse õhku
-        }
+
 
     } // tiksJuhtus
+
+
+    public void sendNewTetromino(int kellele) {
+        char[] possibleTetrominos = {'I', 'O', 'Z', 'S', 'T', 'J', 'L'};
+        Random rand = new Random();
+        char randomTetromino = possibleTetrominos[rand.nextInt(possibleTetrominos.length)];
+
+        for (ServerGameConnectionHandler player : players) {
+            try {
+                DataOutputStream dos = player.getDos();
+                synchronized (dos) {
+                    dos.writeInt(103);
+                    dos.writeInt(kellele);
+                    dos.writeChar(randomTetromino);
+                } // sync
+            } catch (Exception e) { // kui üks mängija läheb katki, siis ei taha me õhku lennata vaid teine mängija jääb üksi mängima
+                ServerMain.debug(1,"Tetromino info saatmisel kasutajale "+player.getUsername()+" läks midagi valesti.");
+                ServerMain.debug(5,e.toString());
+                player.setOpponentID(0);
+                players.remove(player); // teise mängija viskame minema
+            }
+        } // iter
+
+
+
+
+
+
+
+    }
 
 
 } // class
