7f7f39ee945e1d9b36595fa00f6fd4b577f7911f
Ingo Ignašov <ingo.ignasov@gmail.com>
2018-04-29 10:45:47 +0300

    eemaldasin mitte enam vajaminevad classid githubi repost(lokaalselt on need failid igaks juhuks veel olemas)

--- src/main/java/chati_leiutis/TestKlient.java
+++
@@ -1,8 +0,0 @@
-package chati_leiutis;
-
-public class TestKlient {
-    public static void main(String[] args)throws  Exception {
-        Klient klient = new Klient();
-        //klient.go();
-    }
-}
